b66dad329184d7bd76ba866adb6838d23e7706e9
Gabriel Guas <gabrielguas22@gmail.com>
2026-05-27 23:00:04 -0300

    subo back hgome-cliente

--- backend/src/repositories/reserva.repository.js
+++ backend/src/repositories/reserva.repository.js
@@ -1,7 +1,23 @@
-import { Reserva } from '../models/index.model.js';
+import { Actividad, Reserva, Turno } from '../models/index.model.js';
 
 export async function findById(id) {
     return Reserva.findByPk(id);
+}
+
+export async function findByUsuarioId(usuarioId) {
+    return Reserva.findAll({
+        where: { usuario_id: usuarioId },
+        include: [
+            {
+                model: Turno,
+                include: [Actividad]
+            }
+        ],
+        order: [
+            [Turno, 'fecha', 'ASC'],
+            [Turno, 'hora_inicio', 'ASC']
+        ]
+    });
 }
 
 export async function updateEstadoPago(id, estadoPago) {
