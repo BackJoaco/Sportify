2346256315ea4413a3bf4811a3b2adaf05441359
mar <tina_martu@icloud.com>
2026-05-30 15:17:24 -0300

    add set-password route

--- backend/src/routes/usuario.route.js
+++ backend/src/routes/usuario.route.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 
 import * as usuarioController from '../controllers/usuario.controller.js';
 
-import { authMiddleware } from '../middleware/auth.middleware.js';
+import { authMiddleware, esAdministrador } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.get('/perfil', authMiddleware, usuarioController.getProfile);
 router.put('/perfil', authMiddleware, usuarioController.updateProfile);
+router.get('/', authMiddleware, esAdministrador, usuarioController.getUsersExceptAdmins);
+router.post('/empleados', authMiddleware, esAdministrador, usuarioController.registerEmployee);
+router.delete('/empleados/:id', authMiddleware, esAdministrador, usuarioController.deleteEmployee);
+router.post('/empleados/set-password', usuarioController.setContrasena); // 
+router.get('/empleados/:id', authMiddleware, esAdministrador, usuarioController.getEmployee);
+
 
 export default router;
