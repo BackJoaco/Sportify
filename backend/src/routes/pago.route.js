ef5c4355d6f00effc84333145878eb5df63f602a
Joaquin Garicano <joaquingaricano@gmail.com>
2026-07-02 21:47:09 -0300

    Estos hay que rehacerlos completos. Logica compleja.

--- backend/src/routes/pago.route.js
+++ backend/src/routes/pago.route.js
@@ -9,8 +9,8 @@ router.get('/mis-pagos', authMiddleware, esCliente, pagoController.getMisPagos);
 router.post('/sena-reserva', authMiddleware, esCliente, pagoController.pagarSena);
 router.get('/sena-reserva/monto', authMiddleware, esCliente, pagoController.obtenerMontoSenaReserva);
 router.get('/sena-turno/monto', authMiddleware, esCliente, pagoController.obtenerMontoSenaTurno);
-router.post('/suscripcion-mensual/monto', authMiddleware, esCliente, pagoController.obtenerMontoSuscripcionMensual);
-router.post('/suscripcion-mensual', authMiddleware, esCliente, pagoController.pagarSuscripcionMensual);
+router.get('/suscripcion-mensual/monto', authMiddleware, esCliente, pagoController.obtenerMontoSuscripcionMensual); //REHACER COMPLETO
+router.get('/suscripcion-mensual', authMiddleware, esCliente, pagoController.pagarSuscripcionMensual);
 router.post('/sena-presencial', authMiddleware, esEmpleado, pagoController.registrarSenaPresencial);
 
 export default router;
